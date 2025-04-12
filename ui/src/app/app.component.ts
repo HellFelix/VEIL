d62e57ed866413e16f3c65b6c78c6e5a94e07fb4
Felix Hellborg <felix.hellborg.la@gmail.com>
2025-04-12 14:44:11 +0200

    [UI] (REFACTOR & UPDATE): Move from index to App component. Add components for rendering different viewpoints

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,24 +1,52 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { invoke } from "@tauri-apps/api/core";
+import { SettingsComponent } from './settings/settings.component';
+import { RoutingComponent } from './routing/routing.component';
+import { LogsComponent } from './logs/logs.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   greetingMessage = "";
+  mainSwitchActive = false;
+  selectedView: string = "dashboard";
+  views: { [key: string]: any } = {
+    dashboard:  DashboardComponent,
+    settings: SettingsComponent,
+    routing: RoutingComponent,
+    logs: LogsComponent,
+  };
 
-  greet(event: SubmitEvent, name: string): void {
-    event.preventDefault();
+  currentView: any = this.views[this.selectedView];
 
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    invoke<string>("greet", { name }).then((text) => {
-      this.greetingMessage = text;
-    });
+
+  // greet(event: SubmitEvent, name: string): void {
+  //   event.preventDefault();
+
+  //   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  //   invoke<string>("greet", { name }).then((text) => {
+  //     this.greetingMessage = text;
+  //   });
+  // }
+
+  onViewChange() {
+    this.currentView = this.views[this.selectedView]
   }
+
+  onMainSwitchChange() {
+    if (this.mainSwitchActive) {
+      console.log("Activating...");
+    } else {
+      console.log("Shutting Down...");
+    }
+  } 
 }
